6a58c296d1510949f464e37ab75e9b8945e662a2
Lucas.Xu <lucas.xu@appflowy.io>
2024-04-30 19:04:25 +0800

    fix: refresh the recent views after switching to another workspace

--- frontend/appflowy_flutter/lib/mobile/presentation/home/recent_folder/mobile_home_recent_views.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/home/recent_folder/mobile_home_recent_views.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 import 'package:appflowy/generated/flowy_svgs.g.dart';
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/mobile/presentation/bottom_sheet/bottom_sheet.dart';
@@ -10,6 +8,7 @@ import 'package:appflowy/workspace/application/user/user_workspace_bloc.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder/protobuf.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
@@ -30,8 +29,9 @@ class _MobileRecentFolderState extends State<MobileRecentFolder> {
         ),
       child: BlocListener<UserWorkspaceBloc, UserWorkspaceState>(
         listener: (context, state) {
+          // reset the recent views after switching to another workspace
           context.read<RecentViewsBloc>().add(
-                const RecentViewsEvent.fetchRecentViews(),
+                const RecentViewsEvent.resetRecentViews(),
               );
         },
         child: BlocBuilder<RecentViewsBloc, RecentViewsState>(
