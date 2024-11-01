066007e33a916af4df87275caca8beece8d0f70a
Lucas.Xu <lucas.xu@appflowy.io>
2024-11-01 21:07:31 +0800

    feat: click the path name to open the published page

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/pages/sites/published_page/published_view_item.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/pages/sites/published_page/published_view_item.dart
@@ -1,4 +1,6 @@
+import 'package:appflowy/core/helpers/url_launcher.dart';
 import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:appflowy/plugins/shared/share/constants.dart';
 import 'package:appflowy/startup/startup.dart';
 import 'package:appflowy/util/navigator_context_exntesion.dart';
 import 'package:appflowy/workspace/application/action_navigation/action_navigation_bloc.dart';
@@ -34,53 +36,19 @@ class PublishedViewItem extends StatelessWidget {
         // Published page name
         Expanded(
           flex: flexes[0],
-          child: PublishInfoViewItem(
-            extraTooltipMessage: LocaleKeys
-                .settings_sites_publishedPage_clickToOpenPageInApp
-                .tr(),
-            publishInfoView: publishInfoView,
-            onTap: () {
-              context.popToHome();
-
-              getIt<ActionNavigationBloc>().add(
-                ActionNavigationEvent.performAction(
-                  action: NavigationAction(
-                    objectId: publishInfoView.view.viewId,
-                  ),
-                ),
-              );
-            },
-          ),
+          child: _buildPublishedPageName(context),
         ),
 
         // Published Name
         Expanded(
           flex: flexes[1],
-          child: Padding(
-            padding: const EdgeInsets.only(right: 48.0),
-            child: FlowyButton(
-              text: FlowyTooltip(
-                message:
-                    '${LocaleKeys.settings_sites_publishedPage_clickToOpenPageInBrowser.tr()}\n${publishInfoView.info.publishName}',
-                child: FlowyText(
-                  publishInfoView.info.publishName,
-                  fontSize: 14.0,
-                  figmaLineHeight: 18.0,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ),
-          ),
+          child: _buildPublishedName(context),
         ),
 
         // Published at
         Expanded(
           flex: flexes[2],
-          child: FlowyText(
-            formattedDate,
-            fontSize: 14.0,
-            overflow: TextOverflow.ellipsis,
-          ),
+          child: _buildPublishedAt(context),
         ),
 
         // More actions
@@ -88,6 +56,65 @@ class PublishedViewItem extends StatelessWidget {
           publishInfoView: publishInfoView,
         ),
       ],
+    );
+  }
+
+  Widget _buildPublishedPageName(BuildContext context) {
+    return PublishInfoViewItem(
+      extraTooltipMessage:
+          LocaleKeys.settings_sites_publishedPage_clickToOpenPageInApp.tr(),
+      publishInfoView: publishInfoView,
+      onTap: () {
+        context.popToHome();
+
+        getIt<ActionNavigationBloc>().add(
+          ActionNavigationEvent.performAction(
+            action: NavigationAction(
+              objectId: publishInfoView.view.viewId,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildPublishedAt(BuildContext context) {
+    final formattedDate = SettingsPageSitesConstants.dateFormat.format(
+      DateTime.fromMillisecondsSinceEpoch(
+        publishInfoView.info.publishTimestampSec.toInt() * 1000,
+      ),
+    );
+    return FlowyText(
+      formattedDate,
+      fontSize: 14.0,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  Widget _buildPublishedName(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.only(right: 48.0),
+      child: FlowyButton(
+        useIntrinsicWidth: true,
+        onTap: () {
+          final url = ShareConstants.buildPublishUrl(
+            nameSpace: publishInfoView.info.namespace,
+            publishName: publishInfoView.info.publishName,
+          );
+          afLaunchUrlString(url);
+        },
+        text: FlowyTooltip(
+          message:
+              '${LocaleKeys.settings_sites_publishedPage_clickToOpenPageInBrowser.tr()}\n${publishInfoView.info.publishName}',
+          child: FlowyText(
+            publishInfoView.info.publishName,
+            fontSize: 14.0,
+            figmaLineHeight: 18.0,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ),
     );
   }
 }
